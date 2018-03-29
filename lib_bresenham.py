16d4800114a5720d764434bca167acdee013ae10
houdayec <houdayer.corentin@gmail.com>
2018-03-29 19:18:16 +0200

    Drawing snowflake

--- lib_bresenham.py
+++ lib_bresenham.py
@@ -1,8 +1,12 @@
 import cng as cng
 import random as rdn
 import time
+from snowflakes import *
+
+global point
 
 def bresenham(pointA, pointB):
+    points = []
     dx = pointB.x - pointA.x
     dy = pointB.y - pointA.y
     x = pointA.x
@@ -18,8 +22,8 @@ def bresenham(pointA, pointB):
         dec = abs(dx) - 2*abs(dy)
 
         while(int(x) != int(pointB.x)):
-            #pointA.draw()
-            drawPoint(x,y)
+            points.append(Point(x,y))
+            #projection_point(x,y)
             if(dec < 0):
                 dec = dec + 2*abs(dx)
                 y = y + signY
@@ -28,14 +32,14 @@ def bresenham(pointA, pointB):
     else :
         dec = abs(dy) - 2*abs(dx)
         while(int(y) != int(pointB.y)):
-            #pointA.draw()
-            drawPoint(x,y)
+            points.append(Point(x,y))
+            #projection_point(x,y)
             if(dec < 0):
                 dec = dec + 2*abs(dy)
                 x = x + signX
             dec = dec - 2 * abs(dx)
             y = y + signY
-
+    return points
 def drawPoint(x,y):
     global draw_point
     draw_point = cng.circle(x, y, 1,1)
--- snowflakes.py
+++ snowflakes.py
@@ -1,6 +1,7 @@
 import cng as cng
 import random as rdn
 import time
+import math
 from lib_bresenham import *
 
 #classes definition
@@ -8,7 +9,7 @@ class Point:
     def __init__(self,x,y):
         self.x = x
         self.y = y
-        self.identifier = 0
+        self.id = 0
 
     # Fonction qui permet de calculer les nouvelles coordonnées du point projeté dans la viewport
     def compute_values(self):
@@ -30,6 +31,15 @@ class Point:
         (computed_x, computed_y) = self.compute_values()
         cng.obj_move(self.id, computed_x - point_x, computed_y - point_y)
 
+    def moveFall(self):
+        (point_x, point_y) = cng.obj_get_position(self.id)
+        (computed_x, computed_y) = self.compute_values()
+        cng.obj_move(self.id, (computed_x - point_x), (computed_y - point_y)-100)
+    def moveTo(self, x, y):
+        (point_x, point_y) = cng.obj_get_position(self.id)
+        (computed_x, computed_y) = self.compute_values()
+        cng.obj_move(self.id, (computed_x - point_x) + x, (computed_y - point_y) + y)
+
 class Window:
     def __init__(self, x_origin, y_origin, width, height):
         self.x_origin = x_origin
@@ -38,7 +48,6 @@ class Window:
         self.height = height
 
 #Variables globales
-array_points_snowflake = [ Point(50,50), Point(50,100), Point(50,80), Point(30,100), Point(50,80), Point(70,100) ]
 positionXRectangle=None
 positionYRectangle=None
 rectangleID=None
@@ -49,36 +58,88 @@ draw_point=None
 global viewport_x
 global viewport_y
 
-window_width, window_height = 1280, 720
+window_width, window_height = 1000, 600
 window_x, window_y = 0, 0
-viewport_width, viewport_height = 1000, 600
+viewport_width, viewport_height = 600, 400
 viewport_x, viewport_y = 200, 20
+viewport_min_x = viewport_width - viewport_x
+viewport_max_x = viewport_width + viewport_x
+viewport_min_y = viewport_height - viewport_y
+viewport_max_y = viewport_height + viewport_y
+height_snowflake = 40
+width_snowflake = 40
+angle_rotation_snowflake = 1.0472
 
+array_points_branch_snowflake = [
+Point(viewport_max_x/2,viewport_max_y-(height_snowflake/2)), # Base de la tige centrale du flocon
+Point(viewport_max_x/2,viewport_max_y+(height_snowflake/2)), # Haut de la tige centrale du flocon
+Point(viewport_max_x/2,viewport_max_y+(height_snowflake/2) - 20), # Début branche gauche
+Point((viewport_max_x/2) - 20,viewport_max_y+(height_snowflake/2)), # Fin branche gauche
+Point(viewport_max_x/2,viewport_max_y+(height_snowflake/2) - 20), # Début branche droite
+Point((viewport_max_x/2) + 20,viewport_max_y+(height_snowflake/2)) ] # Fin branche droite
 points = []
+array_points_snowflake = []
 
 def drawSnowflake():
-    bresenham(array_points_snowflake[0], array_points_snowflake[1])
-    bresenham(array_points_snowflake[3], array_points_snowflake[2])
-    bresenham(array_points_snowflake[4], array_points_snowflake[5])
+    k = 0
+    point_to_draw = array_points_branch_snowflake
+    for k in range(0,6):
+        if(k != 0): # S'il ne s'agit pas de la première branche, on applique une rotation
+            point_to_draw = rotate_branch(point_to_draw)
+        i = 0
+        for i in range(0,5):
+                print(point_to_draw[i].x, point_to_draw[i].y)
+                print(point_to_draw[i+1].y, point_to_draw[i+1].y)
+                array_points_snowflake.append(bresenham(point_to_draw[i], point_to_draw[i+1]))
+                i = i + 2
+    for list_points in array_points_snowflake:
+        projection_points(list_points)
 
 # Fonction pour projeter un point
-def projection_point(x2 , y2):
-    point = Point(x2, y2)
+def projection_point(point):
     points.append(point)
     point.draw()
+    #print(point.id)
 
 # Fonction pour projeter un ensemble de points
 def projection_points(points):
     for point in points:
-        projection_point(point.x, point.y)
+        projection_point(point)
 
 def main():
     cng.init_window('Snowflakes', window_width, window_height)
     viewport = cng.rectangle(viewport_x, viewport_y, viewport_x + viewport_width, viewport_y + viewport_height)
     pass
 
+def rotate_branch(array_points):
+    new_points = []
+    for point in array_points:
+        new_points.append(rotate(array_points_branch_snowflake[0], point, angle_rotation_snowflake))
+    return new_points
+
+def fall_snowflake():
+    for list_points in array_points_snowflake:
+        for point in list_points:
+            point.moveFall()
+            point.moveTo(50,50)
+
+# Méthode de rotation d'un point par rapport à l'origin d'un certain angle
+def rotate(origin, point, angle):
+    print("origin ", origin.x, " ", origin.y, "point ", point.x, " ",point.y, "angle ", angle)
+    ox, oy = origin.x, origin.y
+    px, py = point.x, point.y
+
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+
+    point = Point(qx,qy)
+    return point
+
 if __name__ == "__main__":
     main()
     drawSnowflake()
-    projection_points(array_points_snowflake)
+    for point in array_points_branch_snowflake:
+        print(point.x, " ", point.y)
+    #projection_points(array_points_branch_snowflake)
+    fall_snowflake()
     cng.main_loop()
